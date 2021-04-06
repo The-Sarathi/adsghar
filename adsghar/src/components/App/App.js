41030b4bcdc7d2bd1a25f500d3bc789a1567c2ea
Shivam Git DB <aryansn0101@gmail.com>
2021-04-06 18:45:56 +0530

    login

--- adsghar/src/components/App/App.js
+++ adsghar/src/components/App/App.js
@@ -6,8 +6,26 @@ import Dashboard from '../Dashboard/Dashboard';
 import Login from '../Login/Login';
 import Preferences from '../Preferences/Preferences';
 
+import useToken from './useToken';
+
+function setToken(userToken) {
+  sessionStorage.setItem('token', JSON.stringify(userToken));
+}
+
+function getToken() {
+  const tokenString = sessionStorage.getItem('token');
+  const userToken = JSON.parse(tokenString);
+  return userToken?.token
+}
+
 function App() {
+
+	  const { token, setToken } = useToken();
+
   const [token, setToken] = useState();
+  const token = getToken();
+
+
 
   if(!token) {
     return <Login setToken={setToken} />
